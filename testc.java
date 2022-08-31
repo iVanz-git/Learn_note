f8d6ee4ef8e49536f579ea4056f3cc2d4c7f00f1
iVanz-git <188808700@qq.com>
2022-08-31 20:04:06 +0800

    git

---
+++ testc.java
@@ -0,0 +1,19 @@
+package Learn_note;
+
+public class testc {
+    public static void main(String[] args ){
+        int a;
+        int b = 10;
+        a = b++;
+        int c = b++;
+        System.out.println(a);
+        System.out.println(c);
+      
+
+
+        
+
+        
+
+    }
+}
